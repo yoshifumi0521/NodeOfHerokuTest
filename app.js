2eaf90df5537c4b80104c547d2b700bfcbcf9e4f
YoshifumiSato <yoshifumi0521@gmail.com>
2013-01-14 11:01:26 +0900

    修正する

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ http.createServer(function (req, res) {
    res.end('Hello World\n');
 
 
-}).listen(1337, "rocky-lake-5452.herokuapp.com");
+}).listen(1337, "127.0.0.1");
 
 console.log('Server running at http://127.0.0.1:1337/');
 
