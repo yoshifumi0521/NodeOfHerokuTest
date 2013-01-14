370b2c6f81458e9c237a25469c42aade6049821a
YoshifumiSato <yoshifumi0521@gmail.com>
2013-01-14 11:09:45 +0900

    app.jsを修正

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 
 var http = require('http');
 
+var port = process.env.PORT || 3000;
+
 http.createServer(function (req, res) {
 
    res.writeHead(200, {'Content-Type': 'text/plain'});
    res.end('Hello World\n');
 
 
-}).listen(30608, "127.0.0.1");
+}).listen(port, "127.0.0.1");
 
 console.log('Server running at http://127.0.0.1:1337/');
 
