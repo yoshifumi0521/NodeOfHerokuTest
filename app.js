cfe86efbc007cc33c0d6d0c186680621d8f3cdc2
YoshifumiSato <yoshifumi0521@gmail.com>
2013-01-14 14:27:10 +0900

    app.jsのsocket.ioの設定を変更

--- app.js
+++ app.js
@@ -16,9 +16,32 @@ console.log('Server running');
 var io = require('socket.io').listen(app);
 io.sockets.on('connection', function (socket) {
 
-   console.log("ソケットを接続"); 
-   console.log(socket);
+   console.log("サーバーサイドのソケット接続成功！"); 
+
+   
+
+
 
 });
+
+//設定 
+io.configure(function () {
+
+   //HerokuではWebSocketがまだサポートされていない？ので、以下の設定が必要 
+   io.set("transports", ["xhr-polling"]);
+   io.set("polling duration", 10); 
+
+   // socket.ioのログ出力を抑制する
+   io.set('log level', 1);
+
+
+
+});
+
+
+
+
+
+
 
 
--- client/client.js
+++ client/client.js
@@ -2,7 +2,7 @@
 $(function() {
 
    //ソケットのオブジェクトを取得。herokuに置いたサーバーに接続した。
-   var socket = io.connect("http://localhost:3000/");
+   var socket = io.connect("http://rocky-lake-5452.herokuapp.com/");
    console.log(socket);
    socket.on('connect', function(){
      
