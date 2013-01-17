fce7d6dcc918504bc8d3708ebedaf2d1620cd484
YoshifumiSato <yoshifumi0521@gmail.com>
2013-01-17 17:20:12 +0900

    修正

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 var http = require('http');
 
-var port = process.env.PORT || 2000;
+var port = process.env.PORT || 3000;
 var app = http.createServer(function (req, res) {
 
    res.writeHead(200, {'Content-Type': 'text/plain'});
